d07aeb6aa0d052b746ae5c1258e1a95f1e62c39d
Raymondl177 <raymondl177@nycstudents.net>
2025-10-30 12:32:11 -0400

    d

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 function dropdownMenu() {
   const quizButton = document.querySelector(".nav-quiz");
   const navFilters = document.querySelector(".quiz-container");
-
   quizButton.addEventListener("click", () => {
-  navFilters.style.display = "flex";
+  navFilters.style.display = navFilters.style.display === "flex" ? "none" : "flex";
   });
 }
 
